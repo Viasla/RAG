f9248a7d9254b312dfce645c0e9597b6179ecffd
Viacheslav Slaboshpytskyi <v.slaboshpytskyi@tippingpoint.works>
2026-08-28 13:00:08 +0200

    changed way files generate

--- makrdowngenerator.py
+++ makrdowngenerator.py
@@ -3,44 +3,51 @@ from pathlib import Path
 import os
 import json
 
+definition_topics = ["Historical Notes", "Intuition", "Formal Definition", "Examples", "Notation", "Interpretation", "Properties"]
+theorem_topics = ["Historical Notes", "Intuition", "Statement", "Proof", "Examples", "Notation", "Interpretation"]
+
 if os.path.exists("datasets.json"):
   datasets = json.load(open("datasets.json", "r"))
-  print("Datasets loaded from memory")
 
 Path("markdown_output").mkdir(parents=True, exist_ok=True)
 
 for name, desc in datasets["definitions"].items():
   try:
+    print(f"Creating file \"{name}\"")
     with open(Path("markdown_output") / (name + ".md"), "w", encoding="utf-8") as f:
-      print("# Historical Notes\n", file=f)
-      for info in desc["Historical Notes"]:
-        print(info, end="\n\n", file=f)
+      print("""---
+tags:
+  - definition
+Type: Definition
+---
+        """, file=f)
 
-      print("# Intuition\n", file=f)
-      for info in desc["Intuition"]:
-        print(info, end="\n\n", file=f)
-
-      print("# Formal Definition\n", file=f)
-      for info in desc["Formal Definition"]:
-        print(info, end="\n\n", file=f)
-
-      print("# Examples\n", file=f)
-      for info in desc["Examples"]:
-        print(info, end="\n\n", file=f)
-
-      print("# Notation\n", file=f)
-      for info in desc["Notation"]:
-        print(info, end="\n\n", file=f)
-
-      print("# Interpretation\n", file=f)
-      for info in desc["Interpretation"]:
-        print(info, end="\n\n", file=f)
-
-      print("# Properties\n", file=f)
-      for info in desc["Properties"]:
-        print(info, end="\n\n", file=f)
+      for topic in definition_topics:
+        print(f"# {topic}", file=f)
+        for infos in desc[topic]:
+          for info in infos["items"]:
+            print(info, end="\n\n", file=f)
+    print("Done")
   except Exception as e:
     print(e)
 
+for name, desc in datasets["theorems"].items():
+  try:
+    print(f"Creating file \"{name}\"")
+    with open(Path("markdown_output") / (name + ".md"), "w", encoding="utf-8") as f:
+      print("""---
+tags:
+  - theorem
+Type: Theorem
+---
+        """, file=f)
 
+      for topic in theorem_topics:
+        print(f"# {topic}", file=f)
+        for infos in desc[topic]:
+          for info in infos["items"]:
+            print(info, end="\n\n", file=f)
+    print("Done")
+  except Exception as e:
+    print(e)
 
